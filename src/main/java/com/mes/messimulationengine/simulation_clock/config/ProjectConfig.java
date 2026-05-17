8c1adb0730e88e7fa0ee47c017832563a741a6b9
Shreyas Desai <ssdesai1@sheffield.ac.uk>
2026-05-17 17:17:14 +0530

    Feat: Update git ignore

---
+++ src/main/java/com/mes/messimulationengine/simulation_clock/config/ProjectConfig.java
@@ -0,0 +1,11 @@
+package com.mes.messimulationengine.simulation_clock.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+
+
+
+}
